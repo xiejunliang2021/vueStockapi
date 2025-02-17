4830dd15a831d88fc7431593a9797513e8a93cea
huabenwuxin <xiejunliang2019@gmail.com>
2025-02-17 16:54:28 +0800

    取消全部认证

--- vueStockapi/settings.py
+++ vueStockapi/settings.py
@@ -136,15 +136,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # DRF配置鉴权方式
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
-    # 'DEFAULT_AUTHENTICATION_CLASSES': (
-    #     'rest_framework_simplejwt.authentication.JWTAuthentication',
-    #     'rest_framework.authentication.SessionAuthentication',
-    # ),
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        'rest_framework.permissions.AllowAny',
     ],
 }
 
