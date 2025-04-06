627f51162b3821ecced7426b1892b33c000caa43
huabenwuxin <xiejunliang2019@gmail.com>
2025-04-06 10:55:51 +0800

    在称重系统中添加了模型的CURD1.0

--- weighing/urls.py
+++ weighing/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import WeighingRecordViewSet
 
 urlpatterns = [
-    path('weighing-records/', WeighingRecordViewSet.as_view(), name='weighing-records'),
+    path('weighing-records/', WeighingRecordViewSet.as_view({'get': 'list', 'post': 'create'}), name='weighing-records'),
+    path('weighing-records/<int:pk>/', WeighingRecordViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='weighing-record-detail'),
 ]
